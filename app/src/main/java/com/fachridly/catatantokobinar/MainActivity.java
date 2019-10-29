00ef9b0bca8eb18d2ada26e2ce815c1e745afe93
Fachri Fadly <fachridly@gmail.com>
2019-10-29 10:17:32 +0700

    Initial application commit

---
+++ app/src/main/java/com/fachridly/catatantokobinar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fachridly.catatantokobinar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
